7b91d0dbe6692e864a1dec3a741352b414b39d95
1ch0 <github1ch0@163.com>
2022-08-12 11:06:31 +0800

    Feat: add Read OS Env

--- pkg/utils/read_env/read_env.go
+++ pkg/utils/read_env/read_env.go
@@ -1,16 +1,20 @@
-package main
+package read_env
 
 import (
 	"fmt"
 	"os"
 )
 
-func main() {
-	environ := os.Environ()
-	for i := range environ {
-		fmt.Println(environ[i])
+// ReadOSEnv print the os env, set the option:"all" or set the env's key wanted.
+func ReadOSEnv(option string) {
+	if option == "all" || option == "" {
+		environ := os.Environ()
+		for i := range environ {
+			fmt.Println(environ[i])
+		}
+	} else {
+		envKey := os.Getenv(option)
+		fmt.Printf("The OS ENV %s is %s\n", option, envKey)
 	}
-	fmt.Println("**************************")
-	goPath := os.Getenv("VERSION")
-	fmt.Printf("GOPATH is %s\n", goPath)
+	return
 }
