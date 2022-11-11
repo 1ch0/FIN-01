7b265d4375612d1795e762134213d9780e2c1226
1ch0 <github1ch0@163.com>
2022-11-11 15:16:30 +0800

    Perf: update scripts/init_ubuntu.sh

--- scripts/init_ubuntu.sh
+++ scripts/init_ubuntu.sh
@@ -29,11 +29,8 @@ alias ka='kubectl apply -f'
 alias kd='kubectl delete -f'
 alias dc='docker-compose'
 
-
 export LANG="en_US.UTF-8" # 设置系统语言为 en_US.UTF-8，避免终端出现中文乱码
 export PS1='[\u@dev \W]\$ ' # 默认的 PS1 设置会展示全部的路径，为了防止过长，这里只展示："用户名@dev 最后的目录名"
-
-
 export WORKSPACE="$HOME/workspace" # 设置工作目录
 export PATH=$HOME/bin:$PATH # 将 $HOME/bin 目录加入到 PATH 变量中
 # Default entry folder
