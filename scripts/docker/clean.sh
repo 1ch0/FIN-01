e07fa365d8ed70b3946c7e7757d827e760d9339a
1ch0 <github1ch0@163.com>
2022-11-14 14:14:29 +0800

    Feat: add scripts/docker/clean

--- scripts/docker/clean.sh
+++ scripts/docker/clean.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
-
+# 停止docker
+docker stop $(docker ps -a | grep "Exited" | awk '{print $1 }')
+# 删除docker
+docker rm $(docker ps -a | grep "Exited" | awk '{print $1 }')
+# 删除images
 docker rmi $(docker images | grep "none" | awk '{print $3}')
